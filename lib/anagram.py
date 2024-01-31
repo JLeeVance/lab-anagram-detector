7a2d40b543c60c5814664a8a65a0d4013f95e0ef
J Vance <justinleevance94@gmail.com>
2024-01-31 09:07:39 -0500

    Wow I did it

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list_of_ana):
+        matched_list = []
+        for word in list_of_ana:
+            if sorted(word) == sorted(self.word):
+                matched_list.append(word)
+        return matched_list
+         
+
+
+
+
+list_of_ana = ['enlists', 'google', 'inlets', 'banana']
+listen = Anagram("listen")
+print(listen.match(list_of_ana))
